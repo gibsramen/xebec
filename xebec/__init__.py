7554fa6a31a7e6a5ec687866b02e074b3abfd985
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-04-01 12:52:19 -0700

    move concatenate fn to xebec

--- xebec/__init__.py
+++ xebec/__init__.py
@@ -1,4 +1,7 @@
+import os
+
 from .src.helper import get_logger
 
 
 __version__ = "0.3.0"
+COOKIE_DIR = os.path.dirname(__file__)
--- xebec/{{cookiecutter.project_name}}/workflow/scripts/concatenate.py
+++ xebec/{{cookiecutter.project_name}}/workflow/scripts/concatenate.py
@@ -3,33 +3,7 @@ from pathlib import PurePath
 import pandas as pd
 
 from xebec import get_logger
-
-
-def concatenate_metric_dataframes(files):
-    """Concatenate results from multiple metrics."""
-    def get_metric_info(f):
-        """Return metric type and metric name as tuple.
-
-        results/*_div/*/*/effect_sizes.tsv
-        """
-        path_parts = PurePath(f).parts
-        return path_parts[2], path_parts[3]
-
-    all_dfs = []
-    all_keys = []
-    for f in files:
-        this_df = pd.read_table(f, sep="\t")
-        this_keys = get_metric_info(f)
-        all_dfs.append(this_df)
-        all_keys.append(this_keys)
-
-    total_df = pd.concat(
-        all_dfs,
-        keys=all_keys,
-        names=["phylogenetic", "diversity_metric"]
-    )
-    total_df = total_df.reset_index(level=("phylogenetic", "diversity_metric"))
-    return total_df
+from xebec.src._concatenate import concatenate_metric_dataframes
 
 
 xebec_logger = get_logger(snakemake.log[0], snakemake.rule)
